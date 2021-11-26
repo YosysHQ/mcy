898eb24957f781ec9ed8c1e6c55fe3c6d1db3c43
Claire Xenia Wolf <claire@clairexen.net>
2021-11-26 20:12:03 +0100

    readthedocs theme
    Signed-off-by: Claire Xenia Wolf <claire@clairexen.net>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,13 +5,13 @@ copyright ='2021 YosysHQ GmbH'
 
 # select HTML theme
 html_theme = 'press'
-html_logo = '../images/logo.png'
-html_favicon = '../images/favico.png'
+html_logo = '../static/logo.png'
+html_favicon = '../static/favico.png'
 html_css_files = ['yosyshq.css']
 html_sidebars = {'**': ['util/searchbox.html', 'util/sidetoc.html']}
 
 # These folders are copied to the documentation's HTML output
-html_static_path = ['_static', "../images"]
+html_static_path = ['../static', "../images"]
 
 # code blocks style 
 pygments_style = 'colorful'
